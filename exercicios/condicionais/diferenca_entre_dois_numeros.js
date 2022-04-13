724d2b9a7344793f43eabad2ff703b3a22e98d67
Madsshow <madsonmdfs@gmail.com>
2022-04-13 20:38:25 -0300

    adicionando exercício de diferença entra dois números

---
+++ exercicios/condicionais/diferenca_entre_dois_numeros.js
@@ -0,0 +1,12 @@
+console.log("Hello Wolrd")
+var idade1 = parseInt(prompt("Digite um número:"));
+var idade2 = parseInt(prompt("Digite outro número:"));
+console.log(idade1)
+console.log(idade2)
+/*Receba dois números e diga qual é o maior */
+if(idade1 > idade2){
+    console.log("O número 1 é maior ")
+}else if(idade2 > idade1){
+    console.log("O número 2 é maior ")
+};
+
